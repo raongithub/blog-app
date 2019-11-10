09ead77f59b79f85aec9327faffb6e4cdc72b703
raongithub <ramuasanongit@gmail.com>
2019-11-10 02:31:01 -0500

    updating app entry point :sparkles:

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express'),
       app     = express()
 
-app.get('/', (req, res) => res.send('Hello Word!'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.redirect('/blogs'))
+
+app.get('/blogs', (req, res) => res.render('index'))
 
 app.listen(3000, () => console.log('server started...'))
