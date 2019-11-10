5a95f0efd158101d257623fe7585231b8a8451eb
raongithub <ramuasanongit@gmail.com>
2019-11-10 03:30:19 -0500

    configuring and updating entry file :construction:

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-const express = require('express'),
-      app     = express()
+const bodyParser       = require('body-parser'),
+      methodOverride   = require('method-override'),
+      expressSanitizer = require('express-sanitizer'),
+      mongoose         = require('mongoose')
+      express          = require('express'),
+      app              = express()
 
+mongoose.connect('mongodb://localhost/blog-app', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+})
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressSanitizer())
+app.use(methodOverride('_method'))
 
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+let Blog = mongoose.model('Blog', blogSchema)
+
+// RESTful ROUTES
 app.get('/', (req, res) => res.redirect('/blogs'))
 
-app.get('/blogs', (req, res) => res.render('index'))
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log('ERROR!')
+        } else {
+            res.render('index', {
+                blogs: blogs,
+            })
+        }
+    })
+})
 
 app.listen(3000, () => console.log('server started...'))
