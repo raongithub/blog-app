24e2f677f9edaf669c68ce12a2971d7e79a0c3cf
raongithub <ramuasanongit@gmail.com>
2019-11-10 10:47:15 -0500

    tweaking app JS file :zap:

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const bodyParser       = require('body-parser'),
       methodOverride   = require('method-override'),
       expressSanitizer = require('express-sanitizer'),
-      mongoose         = require('mongoose')
+      mongoose         = require('mongoose'),
       express          = require('express'),
-      app              = express()
+      app              = express();
 
 // CONFIG APP & MONGOOSE
 mongoose.connect('mongodb://localhost/blog-app', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
-})
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(expressSanitizer())
-app.use(methodOverride('_method'))
+});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
 
 // Create MONGOOSE SCHEMA / MODEL CONFIG
 let blogSchema = new mongoose.Schema({
@@ -29,21 +29,21 @@ let blogSchema = new mongoose.Schema({
 })
 
 // Compile into the model created
-let Blog = mongoose.model('Blog', blogSchema)
+let Blog = mongoose.model('Blog', blogSchema);
 
 // RESTful ROUTES
-app.get('/', (req, res) => res.redirect('/blogs'))
+app.get('/', (req, res) => res.redirect('/blogs'));
 
 app.get('/blogs', (req, res) => {
     Blog.find({}, (err, blogs) => {
         if (err) {
-            console.log('ERROR!')
+            console.log('ERROR!');
         } else {
             res.render('index', {
                 blogs: blogs,
-            })
+            });
         }
-    })
-})
+    });
+});
 
-app.listen(3000, () => console.log('server started...'))
+app.listen(3000, () => console.log('server started...'));
